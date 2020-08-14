8ea8568f584c9c36afa0e7d8c814ba1c0422b24b
Ayano-t <ayano.t0925@gmail.com>
2020-08-14 23:43:47 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,20 +1,25 @@
 class ItemsController < ApplicationController
   def index
-    @items = Item.all
+    @items = Item.all.limit(5)
+    # @images = Images.all
   end
 
   def new
+    @item = Item.new
+  end
+
+  def edit
+    @item = Item.find(params[:id])
   end
 
   def show
     @item = Item.all.limit(5)
-    @large_category = Largecategory.find(@item.large_category_id).name
-    @middle_category = Middlecategory.find(@product.middle_category_id).name
-    @small_category = Smallcategory.find(@product.small_category_id).name
+    # @large_category = Largecategory.find(@item.large_category_id).name
+    # @middle_category = Middlecategory.find(@product.middle_category_id).name
+    # @small_category = Smallcategory.find(@product.small_category_id).name
   end
 
-  def edit
-  end
+  
 
   def destroy
     @item.destroy
